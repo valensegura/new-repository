3be6301777cb9e51f83edec1f29aef25b784a461
valensegura <valentinasegura760@gmail.com>
2022-07-11 15:50:41 -0300

    comentario

---
+++ arrays-objetos/apps.js
@@ -0,0 +1,19 @@
+// const alumna1 = 'Mica';
+// const alumna2 = 'Maria de los angeles';
+// const alumna3 = 'Cecilia';
+
+
+// Posicion visual           1       2        3          4        5    6     7     8        9       10
+const listaestudiantes = ['Mica', 'Maria','cecilia', 'Hannah', 'Caro', 12, true,undefined,null,[1,2,3,4,5]]
+// Posicion Logica          0        1        2          3        4     5    6     7        8      9
+
+
+
+for(let i = 0; i < listaestudiantes.length; i++){ // i=i + l
+    console.log(i)
+}
+
+// console.log(listaestudiantes[0])
+// console.log(listaestudiantes[0])
+// console.log(listaestudiantes[0])
+// console.log(listaestudiantes[0])
---
+++ variables/app.js
@@ -0,0 +1,92 @@
+// palabra reservada, nombre, asignacion y el valor
+
+//const edad = 25;
+
+// una variable jamas puede llevar un numero adelante --> 1edad x
+
+// otro ejemplo con mayusculas tampoco X
+
+// las teclas especiales tampoco jamas deben ir en una variable, solo admite los guiones bajos _
+
+// la primer palabra de la variable no puede ir con mayuscula, pero las siguientes si --> primerApellidoMaterno
+
+//¿Que es una declaracion? --> Const edad
+
+//¿que es una asignacion? --> = 25;
+
+
+// const edad = 25; 
+//    nombre ='jonh'; 
+//    altura = 1.85;
+
+// Tipos de datos primitivos
+
+//string --->texto
+
+//const nombre = 'Valentina'
+const apellido ='Segura'
+const compañera = 'Hannah'
+const compañera2 = 'Caro'
+
+//Number --->numeros
+
+const edad = 25;
+const altura = 1.85;
+const negativo = -10
+
+//boolean
+
+const esEstudiante = true
+const esProfe = false
+
+// undefined
+
+const indefinida =  undefined
+
+//null
+
+const noExiste  = null
+
+console.log('Este es el mensaje de salida')
+
+// cuando yo le pongo comillas '' es texto
+// y cuando yo no le pongo es una variable
+
+const ejemplo1 = '2';
+
+//console.log( typeof Number ('2'))
+//console.log (Number('2') + 1)
+
+//const primerNumero = prompt('Ingresa el primer numero!')
+//const segundoNumero = prompt('Ingresa el primer numero!')
+
+//console.log(primerNumero)
+//console.log(segundoNumero)
+
+//si yo quiero sumar los numeros debo hacerlo asi --> Number('2') Transformandolo
+
+//console.log(typeof String (2))
+
+//Metodo para convertir en string ^
+
+//console.log(Boolean('true'))
+
+//Operadores aritmeticos
+
+// + , - , *, / , %, < , >, >=, <=
+
+// Operador de asignacion ---> =
+
+const nombre = prompt ( 'Ingresa tu nombre'); 
+//const nombre = 'valentina';
+
+//alert('Hola'  +  nombre +  'bienvenido')
+
+console.log('Este es el normal');
+
+console.warn('Advertencia no pegues codigo aqui :/ ');
+
+console.error('El usuario ya ha registrado anteriormente, por favor comunicate con rh');
+
+//Hacer los ejercicios!!? AAAaaaaaa :D
+
